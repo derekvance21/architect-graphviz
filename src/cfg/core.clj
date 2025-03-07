dbaec3152a7f112d0fcfa30ec4770b14e4b2a55e
Derek Vance <derekvance21@gmail.com>
2025-03-06 17:35:43 -0800

    input from source file

--- src/cfg/core.clj
+++ src/cfg/core.clj
@@ -1,7 +1,8 @@
 (ns cfg.core
   (:require
    [ubergraph.core :as uber]
-   [clojure.string :as str])
+   [clojure.string :as str]
+   [clojure.java.io :as io])
   (:gen-class))
 
 (def dbdel-g
@@ -50,6 +51,82 @@ Keywords:
 
 ")
 
+(def packing-source-str
+  "_Packing
+
+|----+------------+------------+------------+--------------+-------------
+|    | Label      | Pass       | Fail       | Type         | Action Name 
+|----+------------+------------+------------+--------------+-------------
+|    |            |            | LOG ERROR  | Calculate    | Transaction Code = \"315\"
+|    |            |            | FAILURE    | Call         | Global Values - Manifesting
+|    | RESTART    |            | FAILURE    | Call         | Reset Global Variables
+|    |            |            | LOG ERROR  | Calculate    | Order Number = \"\"
+|    |            |            | LOG ERROR  | Calculate    | Location = \"\"
+|    |            |            | LOG ERROR  | Calculate    | Printer Name = \"\"
+|    |            |            | LOG ERROR  | Calculate    | HU ID = \"\"
+|    |            |            | LOG ERROR  | Calculate    | Source Location = \"\"
+|    |            |            | LOG ERROR  | Calculate    | Source HU ID = \"\"
+|    |            |            | LOG ERROR  | Calculate    | Destination HU ID = \"\"
+|    |            |            | FAILURE    | Call         | Check Inventory On Fork
+| -- |            |            |            |              | 
+|    | PAKSTATION |            | FAILURE    | Call         | Pack - Pack Station
+|    | SOURCE LP  |            | SWITCH1    | Call         | Pack - Source LP
+|    |            |            | GET ITEM   | Compare      | FLAG Repack Set?
+|    |            | GET ITEM   | SOURCE LP  | Call         | Confirm Repack
+|    | SWITCH1    | CUBBY      | PAKSTATION | Compare      | <F2-Switch>?
+|    | CUBBY      |            | SWITCH2    | Call         | Pack - Cubby
+|    |            |            | GET ITEM   | Compare      | FLAG Repack Set?
+|    |            | GET ITEM   | CUBBY      | Call         | Confirm Repack
+|    | SWITCH2    | SOURCE LP  | PAKSTATION | Compare      | <F2-Switch>?
+| -- Loop through by container
+|    | GET ITEM   |            | LOG ERROR  | Call         | DBExec usp_pak_get_next_item
+|    |            | SRC EMPTY  | ITEM       | Compare      | Item ID = \"\"?
+|    | ITEM       | LOT        |            | Call         | Pack - Item
+|    |            |            | EMPTY CHK  | Compare      | <F1-Cancel>?
+|    |            | SOURCE LP  | CUBBY      | Compare      | FLAG LP Scanned set?
+|    | EMPTY CHK  |            | GET ITEM   | Compare      | FLAG Source Empty set?
+|    |            | LP ERR RES |            | Compare      | FLAG Error Resolution set?
+|    |            | SOURCE LP  | CUBBY      | Compare      | FLAG LP Scanned set?
+|    | LOT        |            | ITEM       | Call         | Pack - Lot
+|    | EXP        | DISP INFO  | LOT SET?   | Call         | Pack - Expiration
+| -- | ITM VERIFY | DISP INFO  |            | Call         | Pack - Verify Item Availability
+|    | DISP INFO  | QTY        | LOG ERROR  | Call         | Pack - Display Info
+|    |            |            | LOT SET?   | Compare      | Expiration Date Control = \"Y\"?
+|    |            |            | EXP        | Compare      | FLAG Single Expiration set?
+|    | LOT SET?   |            | ITEM       | Compare      | Lot Control = \"F\"?
+|    |            | ITEM       | LOT        | Compare      | FLAG Single Lot set?
+|    | QTY        | POST CHECK |            | Call         | Pack - Quantity TX
+|    |            | DISP INFO  | LOG ERROR  | Compare      | <F1-Cancel>?
+|    | POST CHECK | LP ERR RES |            | Compare      | FLAG Error Resolution set?
+|    |            | DEST LP    |            | Compare      | FLAG Container Complete set?
+|    |            | SRC EMPTY  | GET ITEM   | Compare      | FLAG Source Empty set?
+| -- Post pack processing
+| -- All stuff has been packed to container, define LP and print
+|    | DEST LP    |            | LOG ERROR  | Call         | Pack - Destination LP
+|    | PRINT      | COMPLETE   |            | Call         | Pack - Print
+|    |            | RESOLUTION |            | Compare      | <F5-Issue>?
+|    |            | DEST LP    | LOG ERROR  | Compare      | <F1-Cancel>?
+| -- Finished!
+|    | COMPLETE   |            |            | Call         | Pack - Complete
+|    |            |            | GET ITEM   | Compare      | FLAG Source Empty set?
+|    |            | SOURCE LP  | CUBBY      | Compare      | FLAG LP Scanned set?
+| -- Empty LP notification
+|    | SRC EMPTY  |            | SRC CHECK  | Call         | Pack - Source Empty
+|    |            |            | GET ITEM   | Compare      | Source HU ID = \"\"?
+|    |            |            | GET ITEM   | Compare      | Source Location = \"\"?
+|    | SRC CHECK  | SOURCE LP  | CUBBY      | Compare      | FLAG LP Scanned set?
+| -- LP Error Resolution Location
+| -- 2024-07-31 BRIANH failure for Error LP should not go to log error
+|    | LP ERR RES | RESOLUTION | ITEM       | Call         | Pack - Dest LP Error Resolution
+| -- 2024-07-31 BRIANH failure for Error Location should not go to log error
+|    | RESOLUTION |            | PRINT      | Call         | Pack - Error Resolution
+|    |            |            | GET ITEM   | Compare      | FLAG Source Empty set?
+|    |            | SOURCE LP  | CUBBY      | Compare      | FLAG LP Scanned set?
+|    | SUCCESS    |            |            | Return       | PASS
+|    | LOG ERROR  |            |            | Calculate    | Log: Error Occurred in SYS_PO
+|    |            |            |            | Send         | Log Msg to APPLOG1 (Error)
+|    | FAILURE    |            |            | Return       | FAIL")
+
 
 
 (str/split "  | 1    | DB ACTION | Database      | Upd PKD Stage Loc w/Fork ID, HU ID| DONE      | NEXT      "
@@ -97,17 +174,67 @@ Keywords:
 
 (defn diff-details
   [s]
-  (->> (str/split-lines s)
-       (partition-by #(not (re-find #"^-+$" %)))
-       (last)
-       (remove empty?)
-       (map diff-columns)
-       (remove #(when-let [label (:label %)]
-                  (re-find #"--" label)))))
+  (into
+   []
+   (comp
+    (drop-while #(not (re-find #"^-+$" %)))
+    (drop 1)
+    (remove empty?)
+    (map diff-columns)
+    (remove #(when-let [label (:label %)]
+               (re-find #"--" label))))
+   (str/split-lines s))
+  )
 
 (comment
   (diff-details po-diff-str)
-  (diff-details po-diff-str)
+  (into
+   []
+   (comp
+    (drop-while #(not (re-find #"^-+$" %)))
+    (drop 1)
+    (remove empty?)
+    (map diff-columns)
+    (remove #(when-let [label (:label %)]
+               (re-find #"--" label))))
+   (str/split-lines po-diff-str))
+  )
+
+
+(defn source-columns
+  [line]
+  (into {}
+        (remove (comp empty? val))
+        (zipmap
+         [:comment :label :pass :fail :type :action]
+         (rest (mapv str/trim (str/split line #"\|")))))
+  #_(-> (zipmap
+       [:blank :line :label :type :action :pass :fail]
+       (map str/trim (str/split line #"\|")))
+      (->> (remove (comp empty? val))
+           (into {}))
+      (update :line #(Integer/parseInt %))))
+
+
+(defn source-details
+  [s]
+  (into
+   []
+   (comp
+    (drop-while #(not (re-find #"\|----\+" %)))
+    (drop 1)
+    (drop-while #(not (re-find #"\|----\+" %)))
+    (drop 1)
+    (map source-columns)
+    (map-indexed (fn [i m]
+                   (assoc m :line (inc i))))
+    (remove :comment)
+    )
+   (str/split-lines s)))
+
+
+(comment
+  (source-details packing-source-str) 
   )
 
 (defn graph-inits
@@ -212,15 +339,26 @@ Keywords:
   (apply uber/digraph
          (graph-inits (diff-details diff))))
 
+(defn source-graph
+  [source]
+  (apply uber/digraph
+         (graph-inits (source-details source))))
+
 (comment
   (details valid-pallet-str)
   (graph-inits (details valid-pallet-str))
+  (graph-inits (source-details packing-source-str))
   
   )
 
 (defn diff-name
   [diff]
   (re-find #"(?<=Name: ).+" diff))
+
+
+(defn source-name
+ [s]
+  (first (str/split-lines s)))
 
 
 (defn diff-graph-viz
@@ -240,7 +378,24 @@ Keywords:
          {:save {:filename file
                  :format :jpg}}))))))
 
+(defn source-graph-viz
+  ([source]
+   (source-graph-viz source nil))
+  ([source file]
+   (let [g (source-graph source)
+         name (source-name source)]
+     (uber/viz-graph
+      g
+      (merge {:layout :dot
+              :label name
+              :labelloc "t"}
+             (when file
+               {:save {:filename file
+                       :format :png}}))))))
+
 (comment
+  
+  (source-graph-viz packing-source-str "out/packing2.jpg")
 
   (graph-inits (diff-details
                 (slurp "confirm-database-error-retry.txt")))
@@ -290,14 +445,20 @@ Keywords:
 
 (defn -main
   [& args]
-  (if-let [[jpg process] args]
-    (if (or (nil? process) (= process "-"))
-      (diff-graph-viz (slurp *in*) jpg)
-      (diff-graph-viz (slurp process) jpg)
-      
-    )
-    (println "Usage:
-clj -M -m cfg.core OUTPUT_JPG PROCESS_FILE
-clj -M -m cfg.core OUTPUT_JPG [-] # read from stdin")
-  )
-)
+  (if (seq args)
+    (if (= "-s" (first args))
+      (let [[_ out-folder & files] args]
+        (doseq [file files]
+          (let [f (io/file out-folder (.getName (io/file file)))]
+            (io/make-parents f)
+            (source-graph-viz (slurp file) (str f ".png")))))
+      (let [[jpg process] args]
+        (if (or (nil? process) (= process "-"))
+          (diff-graph-viz (slurp *in*) jpg)
+          (diff-graph-viz (slurp process) jpg))))
+    (println
+     (str/join "\n"
+               ["Usage:"
+                "clj -M -m cfg.core OUTPUT_JPG PROCESS_FILE"
+                "clj -M -m cfg.core OUTPUT_JPG [-] # read from stdin"
+                "clj -M -m cfg.core -s OUT_FOLDER [SOURCE_FILE] ..."]))))
